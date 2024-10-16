1f1c2e5ef60d4d275e763819fb80d6baf058bd8a
CougarBellinger <cougarbellinger@gmail.com>
2024-10-16 13:52:38 -0700

    Edited views.py

--- MyPharma/users/views.py
+++ MyPharma/users/views.py
@@ -85,10 +85,8 @@ def logout_view(request):
     logout(request)
     return redirect('login_view')
 
-def contact_view(request):
-    return render(request, 'contact.html')
-    
 @login_required
+def contact_view(request):
     return render(request, 'users/contact.html')
 
 @login_required 
