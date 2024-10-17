dee07d6e9e6d06c16b141a88a908a7336f3a79c8
edengeyer <edengeyer@arizona.edu>
2024-10-17 13:40:47 -0700

    created htmls for pharmacy manager and added views

--- MyPharma/users/views.py
+++ MyPharma/users/views.py
@@ -323,19 +323,6 @@ def patient_management(request):
     patients = Patient.objects.all()
     return render(request, 'users/patient_management.html', {'patients': patients})
 
-# list of low stock medications
-def low_medications_management(request):
-    low_medications = Medications.objects.filter(tablet_count__lt= 120) # filter DB for tablet_count < 120
-    context = {'low_medications': low_medications} # passes dynamic data to template  
-    return render(request, 'users/low_medications_management.html', {'low_medications': low_medications})
-
-# list of expired and expiring soon medications
-def expiring_medications_management(request):
-    expired_medications = Medications.objects.filter(is_expired=True)
-    expiring_soon_medications = Medications.objects.filter(is_expiring_soon=True)
-    context = {'expired_medications': expired_medications, 'expiring_soon_medications': expiring_soon_medications} # passes dynamic data to template  
-    return render(request, 'users/expiring_medications_management.html', {'expired_medications': expired_medications}, {'expiring_soon_medications': expiring_soon_medications})
-
 def edit_patient(request, pk):
     patient = get_object_or_404(Patient, pk=pk)  # Get the patient object by its primary key (pk)
     if request.method == 'POST':
@@ -348,7 +335,6 @@ def edit_patient(request, pk):
 
     return render(request, 'users/edit_patient.html', {'form': form, 'patient': patient})
 
-
 def delete_patient(request, pk):
     patient = get_object_or_404(Patient, pk=pk)  # Get the patient object by its primary key (pk)
     if request.method == 'POST':
@@ -357,3 +343,21 @@ def delete_patient(request, pk):
 
     return render(request, 'users/delete_patient.html', {'patient': patient})
 
+# list of low stock (< 120) medications
+def low_medications_management(request):
+    low_medications = Medications.objects.filter(is_low= True) # filter DB for tablet_count < 120
+    context = {'low_medications': low_medications} # passes dynamic data to template  
+    return render(request, 'users/low_medications_management.html', {'low_medications': low_medications})
+
+# list of orderable (< 50) medications
+def orderable_medications_management(request):
+    orderable_medications = Medications.objects.filter(is_orderable= True) # filter DB for tablet_count < 50
+    context = {'orderable_medications': orderable_medications} # passes dynamic data to template  
+    return render(request, 'users/orderale_medications_management.html', {'orderable_medications': orderable_medications})
+
+# list of expired and expiring soon medications
+def expiring_medications_management(request):
+    expired_medications = Medications.objects.filter(is_expired=True)
+    expiring_soon_medications = Medications.objects.filter(is_expiring_soon=True)
+    context = {'expired_medications': expired_medications, 'expiring_soon_medications': expiring_soon_medications} # passes dynamic data to template  
+    return render(request, 'users/expiring_medications_management.html', {'expired_medications': expired_medications}, {'expiring_soon_medications': expiring_soon_medications})
