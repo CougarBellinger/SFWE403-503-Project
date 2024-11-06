4581c5b21ccb4dc866939038d3d8dfd6acce2157
edengeyer <edengeyer@arizona.edu>
2024-11-05 18:30:22 -0700

    typo

--- MyPharma/users/forms.py
+++ MyPharma/users/forms.py
@@ -43,5 +43,5 @@ class ManualPrescriptionForm(forms.Form):
     medication = forms.ModelChoiceField(queryset=Medications.objects.all(), label='Select Medication')
 
     # typed out fields
-    num_tablets = forms.IntegerField(label='Number of Tablets', min= 1, max = 500)
+    num_tablets = forms.IntegerField(label='Number of Tablets', min_value= 1, max_value = 500)
     prescriber_name = forms.CharField(label='Name of Prescriber', min = 1, max = 100)
