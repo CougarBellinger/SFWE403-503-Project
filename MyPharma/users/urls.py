b1207cd6ee6108b8b4a1f8f261bcb0d31fb283eb
edengeyer <edengeyer@arizona.edu>
2024-11-05 18:09:46 -0700

    made urls

--- MyPharma/users/urls.py
+++ MyPharma/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import User_registration_view, home_view, cashier_home, login_view,logout_view, contact_view, recover_account_view, create_user, manager_home, customer_home, password_change,user_list, first_password_view, create_patient,delete_user,edit_user,recover_user,patient_management,edit_patient,delete_patient,changePassword_view,myprofile_view
+from .views import User_registration_view, home_view, cashier_home, login_view,logout_view, contact_view, recover_account_view, create_user, manager_home, customer_home, password_change,user_list, first_password_view, create_patient,delete_user,edit_user,recover_user,patient_management,edit_patient,delete_patient,changePassword_view,myprofile_view, manual_prescription, prescription_confirmation
 
 
 urlpatterns = [
@@ -30,5 +30,6 @@ urlpatterns = [
     path('delete/<int:pk>/', delete_patient, name='delete_patient'),
     path('changePassword/', changePassword_view, name='changePassword_view'),
     path('myprofile/', myprofile_view, name='myprofile_view'),
-
+    path('manual_prescription/', manual_prescription, name='manual_prescription'),
+    path('prescription_confirmation/', prescription_confirmation, name='prescription_confirmation')
 ]
